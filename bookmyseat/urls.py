c80af9e692f1cd2a677ff07ae8b576a5be6735ec
Aks-887 <ayushkrsingh542005@gmail.com>
2026-07-21 00:49:54 +0530

    Fix image loading in production

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -9,8 +9,15 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
 handler404 = 'movies.views.error_404'
 handler500 = 'movies.views.error_500'
